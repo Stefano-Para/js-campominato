737311c1b511e13c8dec565828ee965b46e16895
Stefano-Para <stefano.zina@gmail.com>
2021-05-05 16:42:53 +0200

    prima parte creazione bombe

--- js/script-minato.js
+++ js/script-minato.js
@@ -1,34 +1,55 @@
-// il computer deve generare 16 numeri casuali tra 1 e 100.
-// I numeri non possono essere duplicati.
-// In seguito deve chiedere all'utente (100 - 16) volte di inserire un numero alla volta, sempre compreso tra 1 e 100.
-// L'utente non può inserire più volte lo stesso numero.
-// Se il numero è presente nella lista dei numeri generati, la partita termina, altrimenti si continua chiedendo all'utente un altro numero.
-// La partita termina quando il giocatore inserisce un numero "vietato" o raggiunge il numero massimo possibile di numeri consentiti.
-// Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l'utente ha inserito un numero consentito.
-
-// le mie VARIABILI 
+// LE MIE ______________ VARIABILI _______________
 var bombs = [];
 var attempts = [];
 var maxAttempts = 84;
-var finalScore;
+var finalScore = 0;
 
-// le mie FUNZIONI 
+// LE MIE ______________ FUNZIONI _______________
 // Funzione che serve a generare un numero random che utilizzerò per la creazione delle bombe
 function getRandomNumber (min, max) {
     return parseInt(Math.random() * (max - min + 1) + min);
 }
-// var prova = getRandomNumber(1, maxAttempts);
-// console.log("Console log di prova per function getRandomNumber(1, maxAttemps) = " + prova)
-
-// funzione che serve a verificare se un elemento è presente in un array 
-var x = 0;
-
-function isInArray (x, y) {
-    x = y.includes(element);
-    y = array;
+// funzione che serve a VERIFICA se un elemento è presente in un array. Ritornerà TRUE se è contenuto, FALSE se non è contenuto.
+function isInArray (element, array) {
+    var inArray = array.includes(element);
+    return inArray;
 }
+// TEST FUNZIONE IsInArray + IF
+// var x = [1,2,3,4,5];
+// var y = 2;
+// var check = isInArray(0, x);
+// console.log(check)
+//      if (check == true) {
+//      alert("Si è contenuto!")
+//      } else {
+//     alert("Non è contenuto.")
+//      }
 
-var array = [1, 3, 4, 5];
-console.log("Array è composto da " + array)
-var prova = isInArray (3, array);
-console.log("Questo è il log di isInArray (3, array)");
+//\\  CONSEGNA //\\
+
+// 1) Il computer deve generare 16 numeri NON DUPLICATI tra 1 e 100.
+// while non si ferma fino a che non ci sono 16 elementi all'interno dell'array BOMBS 
+
+while (bombs.length < 16)  {
+    createdBomb = getRandomNumber(1, 100);
+    // condizione tramite la quale finchè si genera un numero già presente nell'array (già generato) continuerà a generare numeri (fino a 16 - bombe)
+    if (!isInArray(createdBomb, bombs)) {
+        bombs.push(createdBomb);
+    }
+} 
+console.log("Queste sono le bombe:");
+console.log(bombs); 
+
+// 2) Il pc deve chiedere all'utente (100-16) volte di inserire un numero alla volta sempre compreso tra 1 e 100 e anche in questo caso NON DOPPIO (finchè l'utente inserisce un numero già inserito, continua a chiedere = DO WHILE).
+
+
+// 3) Se il numero inserito è presente nella lista dei numeri generati (check in array) la partita termina sennò si continuano a chiedere numeri fino a "completarli" .
+
+// 4) la partita termina quando l'user inserisce un numero vietato oppure raggiunge il maxAttempts. 
+
+// 5) stampare in console o pagina o con alert quanti tentativi ha effettutato l'utente prima di raggiungere il game over 
+
+
+
+
+
